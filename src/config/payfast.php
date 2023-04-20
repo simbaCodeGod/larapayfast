995e3e7cb6c639bfbbc97b6f6fb6b0ace3b8edd2
Simba <isarawadano1@gmail.com>
2023-04-20 12:56:04 +0200

    update config

--- src/config/payfast.php
+++ src/config/payfast.php
@@ -14,11 +14,11 @@ return
 [
     'testing'  => true, // Set to false when in production.
     'merchant' => [
-        'merchant_id'  => env('PF_MERCHANT_ID', '10000100'),                        // TEST Credentials. Replace with your merchant ID from Payfast.
-        'merchant_key' => env('PF_MERCHANT_KEY', '46f0cd694581a'),                  // TEST Credentials. Replace with your merchant key from Payfast.
-        'return_url'   => env('PF_RETURN_URL', 'http://your-domain.co.za/success'), // Redirect URL on Success.
-        'cancel_url'   => env('PF_CANCEL_URL', 'http://your-domain.co.za/cancel'),  // Redirect URL on Cancellation.
-        'notify_url'   => env('PF_ITN_URL', 'http://your-domain.co.za/itn'),        // ITN URL.
+        'merchant_id'  => env('PF_MERCHANT_ID'),                        // TEST Credentials. Replace with your merchant ID from Payfast.
+        'merchant_key' => env('PF_MERCHANT_KEY'),                  // TEST Credentials. Replace with your merchant key from Payfast.
+        'return_url'   => env('PF_RETURN_URL'), // Redirect URL on Success.
+        'cancel_url'   => env('PF_CANCEL_URL'),  // Redirect URL on Cancellation.
+        'notify_url'   => env('PF_ITN_URL'),        // ITN URL.
     ],
 
     'hosts' => [
